41b2ac0961dc36dd8634b6d7366fcb7637a8e716
ArlisonAndrade <Arlison2012@hotmail.com>
2026-06-05 22:11:06 -0300

    Forçando URL

--- src/App.jsx
+++ src/App.jsx
@@ -899,7 +899,7 @@ export default function App() {
               };
             });
             try {
-              const resp = await fetch("/api/salvar", {
+              const resp = await fetch("https://foal-app.vercel.app/api/salvar", {
                 method: "POST", headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ registros }),
               });
